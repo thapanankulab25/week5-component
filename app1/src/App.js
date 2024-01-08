edcf29f5101775c53e6a7ccf6654dffa9d0bd784
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-01-08 10:44:43 +0700

    insert and update component

---
+++ app1/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Content,Footer, Header } from './func-components'
+function App() {
+
+return (
+[<Header />,<Content /> ,<Footer /> ]
+)
+}
+export default App
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import React from'react'
+// import './style.css';
+// function App() {
+
+//     let r='<b>React</b>'
+//     let rn='<b>React</b> Native'
+//     const el=(
+//       <div>
+//         Create Web <br/>
+//         with <span dangerouslySetInnerHTML={{__html:r}}/> & <span dangerouslySetInnerHTML={{__html:rn}}/>
+//       </div>
+//     )
+// return el
+
+// return(
+//   <table border="1">
+//   <tr>
+//     <th>Product</th><th>Price</th>
+//     </tr>
+//   <tr>
+//     <td>Book</td><td>320</td>
+//   </tr>
+//   <tr>
+//     <td>Cake</td><td>70</td>
+//   </tr>
+//   </table>
+// )
+
+// const divStyle ={
+//   color:'red',
+//   backgroundColor:'powderblue',
+//   fontSize:'larger',
+//   padding:'3px',
+// }
+// return <div style={divStyle}>Hello React</div>
+// return(
+// <>
+// <h2 style={{textAlign: "center"}}>Bitkub.com</h2>
+// <div className='title' style={{textAlign: "center"}}>ยินดีต้องรับสู่นวัตกรรมเปลี่ยนโลก</div>
+// </>
+// )
+// }
+
+// export default App;
---
+++ app1/src/func-components.js
@@ -0,0 +1,35 @@
+import React from 'react'
+export function Header() {
+const aStyle = {
+display: 'inline-block',
+color: 'blue',
+margin: '7px',
+textDecoration: 'none' 
+}
+return (
+    <div style={{backgroundColor: '#ccc'}}>
+    <a href="/home" style={aStyle}>Home</a> |&nbsp;
+    <a href="/react" style={aStyle}>React</a> |&nbsp;
+    <a href="/react Native" style={aStyle}>React Native</a>
+    </div>
+    )
+}
+export function Content() 
+{
+    return (
+    <div style={{textAlign: 'center'}}>
+    <h2>Hello <br/>React & React Native</h2>
+    <br/><br/><br/>
+    </div>
+    )
+    }
+    export const Footer = () => {
+    return (
+    <div style={{textAlign: 'center'}}>
+    &copy; {new Date().getFullYear()} 2022 All right reserved
+    </div>
+    )
+}
+    
+
+    
